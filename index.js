851bb47a87a5cc6add484f4880e63957ffa3e7cc
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-09-01 19:00:27 +0600

    Practice Module 30

---
+++ index.js
@@ -0,0 +1,9 @@
+const users = [
+	{ id: 1, name: "Alice", age: 22 },
+	{ id: 2, name: "Bob", age: 17 },
+	{ id: 3, name: "Charlie", age: 25 },
+];
+
+users.map((user) => {
+	console.log(user.name);
+});
